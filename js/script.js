203b00840c4f63d754aba5047449e13a7c0c4def
ricomf <139717639+ricomf@users.noreply.github.com>
2023-08-18 11:21:03 -0300

    primeiro-commit-pokedex-incompleta
    Falta apenas configurar os botões, porém a barra de pesquisa está funcionando

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+
+const fetchPokemon = async (pokemon) => {
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+const data = await APIResponse.json();
+return data;
+}
+
+const renderPokemon = async(pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny']
+
+}
+
+form.addEventListener('submit', (event) =>{
+
+event.preventDefault();
+
+renderPokemon(input.value.toLowerCase());
+input.value = '';
+});
